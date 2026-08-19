8cee070359d186894d845df709e7b3aca9b47e66
Mamat Rahmat <coderbodoh@gmail.com>
2026-08-19 14:48:01 +0700

    feat: dispatch all 8 lesson block types in LessonBlockRenderer

--- resources/js/components/lesson/LessonBlockRenderer.vue
+++ resources/js/components/lesson/LessonBlockRenderer.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import type { LessonBlock } from '@/types/lesson';
+import CodeChallengeBlock from './CodeChallengeBlock.vue';
 import CodeExampleBlock from './CodeExampleBlock.vue';
+import CodeFillBlock from './CodeFillBlock.vue';
+import CodeReorderBlock from './CodeReorderBlock.vue';
+import HintBlock from './HintBlock.vue';
+import McqMultipleBlock from './McqMultipleBlock.vue';
 import McqSingleBlock from './McqSingleBlock.vue';
 import TextBlock from './TextBlock.vue';
 
@@ -17,8 +22,34 @@ defineProps<{
         :content="block.content"
     />
 
+    <HintBlock v-else-if="block.type === 'HINT'" :content="block.content" />
+
     <McqSingleBlock
         v-else-if="block.type === 'MCQ_SINGLE'"
+        :block-id="block.id"
+        :content="block.content"
+    />
+
+    <McqMultipleBlock
+        v-else-if="block.type === 'MCQ_MULTIPLE'"
+        :block-id="block.id"
+        :content="block.content"
+    />
+
+    <CodeFillBlock
+        v-else-if="block.type === 'CODE_FILL'"
+        :block-id="block.id"
+        :content="block.content"
+    />
+
+    <CodeReorderBlock
+        v-else-if="block.type === 'CODE_REORDER'"
+        :block-id="block.id"
+        :content="block.content"
+    />
+
+    <CodeChallengeBlock
+        v-else-if="block.type === 'CODE_CHALLENGE'"
         :block-id="block.id"
         :content="block.content"
     />
