d976e910360fe813977af61c45c5d5b2359d58fe
Mamat Rahmat <coderbodoh@gmail.com>
2026-08-19 14:54:11 +0700

    feat: add Courses menu to sidebar and is_admin to User type

--- resources/js/types/auth.ts
+++ resources/js/types/auth.ts
@@ -4,6 +4,7 @@ export type User = {
     email: string;
     avatar?: string;
     email_verified_at: string | null;
+    is_admin: boolean;
     created_at: string;
     updated_at: string;
     [key: string]: unknown;
